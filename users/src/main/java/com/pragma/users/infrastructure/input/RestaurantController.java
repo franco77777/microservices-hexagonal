426b43433ed60be4be6b3c27dd47dc52f6aee498
franco7777777 <francoaresis@gmail.com>
2023-02-22 20:53:26 -0300

    circuit breaker load

--- users/src/main/java/com/pragma/users/infrastructure/input/RestaurantController.java
+++ users/src/main/java/com/pragma/users/infrastructure/input/RestaurantController.java
@@ -4,8 +4,10 @@ import com.pragma.users.application.handler.IObjectHandler;
 import com.pragma.users.application.response.UserResponseDto;
 import com.pragma.users.domain.model.Square.RestaurantModel;
 import com.pragma.users.infrastructure.output.services.RestaurantService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +21,7 @@ import java.util.List;
 public class RestaurantController {
 @Autowired
     RestaurantService restaurantService;
-
+   @CircuitBreaker(name = "squareCB", fallbackMethod = "fallbackGetRestaurant")
    @GetMapping("/{id}")
     public ResponseEntity<List<RestaurantModel>> getByUserId(@PathVariable("id") Long userId){
        List<RestaurantModel> restaurants = restaurantService.getByUserId(userId);
@@ -37,6 +39,9 @@ public class RestaurantController {
 
         RestaurantModel restaurantNew = restaurantService.createRestaurant(userId, restaurant);
         return ResponseEntity.ok(restaurantNew);
+    }
+    public ResponseEntity<List<RestaurantModel>>fallbackGetRestaurant(@PathVariable("id") Long userId, RuntimeException e){
+       return new ResponseEntity("error in the database square whit the user" + userId, HttpStatus.OK);
     }
     }
 
