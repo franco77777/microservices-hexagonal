25ee5b6f7f860a3a87c3a87c829cacee1863f714
franco7777777 <francoaresis@gmail.com>
2023-02-24 17:23:39 -0300

    jwt 1

---
+++ auth-service/src/main/java/com/powerup/authservice/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.powerup.authservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic(Customizer.withDefaults())
+                .build();
+
+    }
+}
